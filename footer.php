c8517618915dd575e97a8c8c44fc9b0d6caabb06
mainak-skilltechlab <mainak.banerjee@skilltechlab.com>
2022-03-08 16:36:55 +0530

    website-main-git-upload

---
+++ footer.php
@@ -0,0 +1,74 @@
+
+  <!-- font-awesome -->
+  <link rel="stylesheet" href="plugins/font-awesome/css/font-awesome.css">
+
+<!-- footer -->
+<footer class="footer-section footer" style="background-image: url(images/backgrounds/footer-bg.png);">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 text-center text-lg-left mb-4 mb-lg-0">
+        <!-- logo -->
+        <a href="index.html">
+          <img class="img-fluid" src="images/logo.png" alt="logo">
+        </a>
+      </div>
+      <!-- footer menu -->
+      <nav class="col-lg-8 align-self-center mb-5">
+        <ul class="list-inline text-lg-right text-center footer-menu">
+          <li class="list-inline-item active"><a href="#">Home</a></li>
+          <li class="list-inline-item"><a  href="#">Feature</a></li>
+          <li class="list-inline-item"><a href="#">About</a></li>
+          <li class="list-inline-item"><a  href="#">Services</a></li>
+          <li class="list-inline-item"><a href="#">Team</a></li>
+          <li class="list-inline-item"><a href="#">Contact</a></li>
+          <li class="list-item"><a href="#">KB21, Saltlake Sector 3,Kolkata 700098,near by AMRI Hospital&nbsp;<i class="fa fa-map-marker"></i></a></li>
+          <li class="list-inline-item"><a href="#"><i class="fa fa-envelope"></i>&nbsp;info@skilltechlab.com</a></li>
+          <li class="list-inline-item"><a href="#"><i class="fa fa-phone"></i>&nbsp;+91 75960 32211</a></li>
+        </ul>
+      </nav>
+      
+      <!-- footer social icon -->
+      <nav class="col-12">
+        <ul class="list-inline text-lg-right text-center social-icon">
+          <li class="list-inline-item">
+            <a class="facebook" href="#"><i class="ti-facebook"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="twitter" href="#"><i class="ti-twitter-alt"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="linkedin" href="#"><i class="ti-linkedin"></i></a>
+          </li>
+          <li class="list-inline-item">
+            <a class="black" href="#"><i class="ti-instagram"></i></a>
+          </li>
+        </ul>
+        
+                       
+        <br><p style="text-align:center;">Copyright @ 2022 skilltechlab Pvt. Ltd.| All Rights Reserved </p>
+       
+      
+      </nav>
+      
+      <!--copyright-->
+      
+    </div>
+  </div>
+</footer>
+<!-- /footer -->
+
+<!-- jQuery -->
+<script src="plugins/jQuery/jquery.min.js"></script>
+<!-- Bootstrap JS -->
+<script src="plugins/bootstrap/bootstrap.min.js"></script>
+<!-- slick slider -->
+<script src="plugins/slick/slick.min.js"></script>
+<!-- venobox -->
+<script src="plugins/Venobox/venobox.min.js"></script>
+<!-- aos -->
+<script src="plugins/aos/aos.js"></script>
+<!-- Main Script -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
